322c6b7bf13141247a0fc7bdd6b2c409b4c2cb98
Art <vitaminearte@gmail.com>
2023-08-30 17:18:49 +0300

    Finished Gatsby Tuturial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+
+function AboutPage() {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+  );
+}
+
+export const Head = () => <title>About Me</title>;
+
+export default AboutPage;
